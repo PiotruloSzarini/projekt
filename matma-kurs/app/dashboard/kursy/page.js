854ef41ee5effc026871f665e5224096bb60088f
Kacpi476 <88859642+Kacpi476@users.noreply.github.com>
2025-12-26 18:39:51 +0100

    kurs -> rodzial update

--- matma-kurs/app/dashboard/kursy/page.js
+++ matma-kurs/app/dashboard/kursy/page.js
@@ -1,6 +1,7 @@
 import CourseCard from '../components/CourseCard/CourseCard';
 import styles from '../kursy/page.module.css'
 import courses from '@/dane/courses.json';
+import Link from 'next/link';
 
 export default function KursyPage() {
     const ownedCourses = courses.filter(c => c.userState.owned);
@@ -8,35 +9,47 @@ export default function KursyPage() {
 
     return (
         <div className={styles.page_container}>
-        <p>Posiadane:</p>
-        <div className={styles.course_container}>
-            {ownedCourses.map(course => (
-            <CourseCard
-                key={course.courseId}
-                title={course.title}
-                backgroundColor={course.color}
-                tasksCount={course.stats.tasksCount}
-                videosCount={course.stats.videosCount}
-                progress={course.userState.progress}
-                owned={true}
-            />
-        ))}
-        </div>
+            <p>Posiadane:</p>
 
-        <p>Nie posiadane:</p>
-        <div className={styles.course_container}>
-            {notOwnedCourses.map(course => (
-            <CourseCard
+            <div className={styles.course_container}>
+
+                {ownedCourses.map(course => (
+
+                <Link
                 key={course.courseId}
-                title={course.title}
-                backgroundColor={course.color}
-                tasksCount={course.stats.tasksCount}
-                videosCount={course.stats.videosCount}
-                progress={0}
-                owned={false}
-            />
+                href={`/dashboard/kursy/${course.slug}`}
+                className={styles.courseLink}
+                >
+
+                <CourseCard
+                    key={course.courseId}
+                    title={course.title}
+                    backgroundColor={course.color}
+                    tasksCount={course.stats.tasksCount}
+                    videosCount={course.stats.videosCount}
+                    progress={course.userState.progress}
+                    owned={true}
+                />
+                </Link>
+
             ))}
-        </div>
+            </div>
+
+
+            <p>Nie posiadane:</p>
+            <div className={styles.course_container}>
+                {notOwnedCourses.map(course => (
+                <CourseCard
+                    key={course.courseId}
+                    title={course.title}
+                    backgroundColor={course.color}
+                    tasksCount={course.stats.tasksCount}
+                    videosCount={course.stats.videosCount}
+                    progress={0}
+                    owned={false}
+                />
+                ))}
+            </div>
     </div>
     );
 }
