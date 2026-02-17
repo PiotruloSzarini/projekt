b92031382bf38d4b3b62f38b7bf77b2ceb9758bb
PiotruloSzarini <piotr.szar@vp.pl>
2026-02-17 15:22:52 +0100

    poprawka dla wtyczek

--- matma-kurs/app/layout.js
+++ matma-kurs/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-    <html>
+    <html lang="pl" suppressHydrationWarning>
         <body className={instrumentSans.variable}>
             {children}
         </body>
